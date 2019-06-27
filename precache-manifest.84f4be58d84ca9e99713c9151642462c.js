b153493e70f93571f0b6546d0dd007969afc96ac
Joseph Davis <joseph.davis@hrblock.com>
2019-06-27 09:14:42 -0500

    Updates

---
+++ precache-manifest.84f4be58d84ca9e99713c9151642462c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22858c302e54eff5b497b629187ae5d9",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "935bba6805a980b99aa1",
+    "url": "/random-quote-machine/static/css/2.43d354e1.chunk.css"
+  },
+  {
+    "revision": "1b66b24e5fcbbc6de5e5",
+    "url": "/random-quote-machine/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "935bba6805a980b99aa1",
+    "url": "/random-quote-machine/static/js/2.68ece01e.chunk.js"
+  },
+  {
+    "revision": "1b66b24e5fcbbc6de5e5",
+    "url": "/random-quote-machine/static/js/main.da5eefa3.chunk.js"
+  },
+  {
+    "revision": "6fefced6fd426e504746",
+    "url": "/random-quote-machine/static/js/runtime~main.296aa1cf.js"
+  }
+]);
